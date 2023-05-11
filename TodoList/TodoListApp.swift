91ec1295cee32aa1c6d131b2aa88230b68122bb9
Taner Celik <tanercelik2001@gmail.com>
2023-05-12 00:15:42 +0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Taner Çelik on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
